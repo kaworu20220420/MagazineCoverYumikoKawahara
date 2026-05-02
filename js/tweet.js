6de47f566a9401c40fbef558a95ef991f3ebbaad
KaoruMisaki@tagRenamer <auto@example.com>
2026-05-02 18:13:13 +0900

    自動更新 - 雑誌名: 週刊少女コミック - 号数: 新春3号 - 発行日: 1980年02月05日 - コミット日時: 2026/05/02 18:13:13

--- js/tweet.js
+++ js/tweet.js
@@ -1,6 +1,6 @@
 	document.getElementById("tweetButton").addEventListener("click", function () {
         const pageUrl = encodeURIComponent(window.location.href);
-	const text = encodeURIComponent("川原由美子 雑誌表紙ギャラリー");
+	const text = encodeURIComponent("川原由美子 雑誌表紙ギャラリー\n @misakikaoru \n");
 	const tweetUrl = `https://twitter.com/intent/tweet?text=${text}&url=${pageUrl}`;
 	window.open(tweetUrl, "_blank");
     });
